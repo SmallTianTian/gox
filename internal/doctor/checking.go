9bd13e9309abdb592ad4c314c3730437995fa533
tick <tianxuxin@126.com>
2021-05-24 17:37:35 +0800

    feat: add more doctor.

--- internal/doctor/checking.go
+++ internal/doctor/checking.go
@@ -1,9 +1,9 @@
 package doctor
 
 import (
-	"os"
 	"os/exec"
-	"path/filepath"
+	"regexp"
+	"strconv"
 
 	"github.com/SmallTianTian/fresh-go/utils"
 )
@@ -15,15 +15,19 @@ func CheckingEnv() {
 		IsMust bool
 		F      func() (bool, string)
 	}{{
-		"Go", "Go is the foundation of your programming.", true, CheckGo,
+		"Go", "Go 是你开始写程序的基础，但我们需要版本大于 1.16。", true, CheckGo,
 	}, {
-		"Make", "Make helps you run commands quickly.", true, CheckMake,
+		"Make", "Make 将帮助你更便捷的运行命令。", true, CheckMake,
 	}, {
-		"Docker", "Docker helps you build image.", false, CheckDocker,
+		"Docker", "Docker 能帮助你生成不依赖本地环境的镜像。", false, CheckDocker,
 	}, {
-		"Grpc", "Grpc is the fast protocol.", false, CheckGrpcCompile,
+		"Grpc", "Grpc 是一个高效的通讯协议，需要依据 proto 文件编译具体代码。", false, CheckGrpcCompile,
 	}, {
-		"Grpc gateway", "Grpc gatewat helps you convert http to grpc.", false, CheckGrpcGatewayCompile,
+		"Grpc gateway", "Grpc gatewat 提供了 http 协议和 gRpc 协议转换的能力，同样需要根据 proto 编译。", false, CheckGrpcGatewayCompile,
+	}, {
+		"Buf", "Buf 是一个简化编译 grpc proto 的工具。", false, CheckBuf,
+	}, {
+		"Wire", "Wire 是一个依赖注入框架，能便捷的生成代码。", false, CheckWire,
 	}}
 
 	for _, v := range checks {
@@ -50,7 +54,11 @@ func CheckGo() (bool, string) {
 	if err != nil {
 		return false, ""
 	}
-	return true, string(bs)
+	versionR := regexp.MustCompile(`\d+.\d+`)
+	v := versionR.FindString(string(bs))
+	vf, _ := strconv.ParseFloat(v, 64) // nolint
+
+	return vf >= 1.16, string(bs) // nolint
 }
 
 func CheckMake() (bool, string) {
@@ -79,13 +87,9 @@ func CheckGrpcCompile() (bool, string) {
 More doc: https://grpc.io/docs/languages/go/quickstart/`
 	}
 
-	var gp string
-	if gp = os.Getenv("GOPATH"); gp == "" {
-		gp = os.Getenv("HOME") + "/go"
-	}
+	exist1 := utils.CheckCommandExists("protoc-gen-go")
+	exist2 := utils.CheckCommandExists("protoc-gen-go-grpc")
 
-	exist1 := utils.IsExist(filepath.Join(gp, "bin", "protoc-gen-go"))
-	exist2 := utils.IsExist(filepath.Join(gp, "bin", "protoc-gen-go-grpc"))
 	if exist1 && exist2 {
 		return true, string(bs)
 	}
@@ -101,18 +105,32 @@ func CheckGrpcGatewayCompile() (bool, string) {
 More doc: https://github.com/grpc-ecosystem/grpc-gateway`
 	}
 
-	var gp string
-	if gp = os.Getenv("GOPATH"); gp == "" {
-		gp = os.Getenv("HOME") + "/go"
-	}
-
-	exist1 := utils.IsExist(filepath.Join(gp, "bin", "protoc-gen-grpc-gateway"))
-	exist2 := utils.IsExist(filepath.Join(gp, "bin", "protoc-gen-openapiv2"))
-	exist3 := utils.IsExist(filepath.Join(gp, "bin", "protoc-gen-go"))
-	exist4 := utils.IsExist(filepath.Join(gp, "bin", "protoc-gen-go-grpc"))
+	exist1 := utils.CheckCommandExists("protoc-gen-grpc-gateway")
+	exist2 := utils.CheckCommandExists("protoc-gen-openapiv2")
+	exist3 := utils.CheckCommandExists("protoc-gen-go")
+	exist4 := utils.CheckCommandExists("protoc-gen-go-grpc")
 	if exist1 && exist2 && exist3 && exist4 {
 		return true, string(bs)
 	}
 	return false, `Cloudn't compile grpc gateway.
 More doc: https://github.com/grpc-ecosystem/grpc-gateway`
+}
+
+func CheckBuf() (bool, string) {
+	cmd := exec.Command("buf", "--version")
+	bs, err := cmd.Output()
+	if err != nil {
+		return false, `Cloudn't call buf.
+More doc: https://docs.buf.build/installation/#from-source`
+	}
+	return true, string(bs)
+}
+
+func CheckWire() (bool, string) {
+	exist := utils.CheckCommandExists("wire")
+	if exist {
+		return true, ""
+	}
+	return false, `Cloudn't exist command wire.
+More doc: https://github.com/google/wire`
 }
